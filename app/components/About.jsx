aa5eaf39afc28ca60e268b8e2ee492cc7344cd1d
Vipul Kumar <tovipul.kr@gmail.com>
2025-12-15 23:38:04 +0530

    Update project count in About component

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -7,7 +7,7 @@ const About = ({ isDarkMode }) => {
     const infoList = [  
         { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'HTML,CSS,JavaScript,C, C++,Java,Python.' },
         { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.Tech in Computer Science' },
-        { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects' }
+        { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 9 projects' }
     ];
     
     const toolsData = [
